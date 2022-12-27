a9aa0b388e13311e92925498d51e9c714d69d500
Stefan Hagen <stefan@hagen.link>
2022-12-27 13:23:40 +0100

    Some modified emptiness

--- hyperkuutio/__init__.py
+++ hyperkuutio/__init__.py
@@ -1,3 +1,8 @@
 """Hypercube (Finnish: hyperkuutio) tool that scratches some itch."""
-__version__ = '2022.2.19'
-__version_info__ = tuple(__version__.split('.'))
+
+# [[[fill git_describe()]]]
+__version__ = '2022.12.13+parent.ff7049ef'
+# [[[end]]] (checksum: 853a9f21e62834d23aa0601ad62a61ce)
+__version_info__ = tuple(
+    e if '-' not in e else e.split('-')[0] for part in __version__.split('+') for e in part.split('.') if e != 'parent'
+)
---
+++ hyperkuutio/cli.py
@@ -0,0 +1,6 @@
+from typing import no_type_check
+
+
+@no_type_check
+def app(argv=None, prog_name=None):
+    return 0
